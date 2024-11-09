6ee60c3a8d65404a77a50581708aab5ab36c6d6c
rukavain <magtoto599@gmail.com>
2024-11-09 18:27:03 +0800

    Online Payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -15,44 +15,6 @@ class PaymentController extends Controller
 
         dd("asd");
 
-        $data = [
-            'data' => [
-                'attributes' => [
-                        'line_items' => [
-                                [
-                                    'currency' => 'PHP',
-                                    'amount' => 10000,
-                                    'description' => 'text',
-                                    'name' => 'Test Product',
-                                    'quantity' => 1,
-                                ]
-                            ],
-                        'payment_method_types' => [
-                            'card',
-                        ],
-                        'success_url' => 'https://localhost:8000/success',
-                        'cancel_url' => 'http://localhost:8000/cancel',
-                        'description' => 'text',
-                    ]
-                ]
-
-            ];
-
-
-        $response = Curl::to("https://api.paymongo.com/v1/checkout_sessions")
-            ->withHeader('Content-Type: application/json')
-            ->withHeader('accept: application/json')
-            ->withHeader('Authorization: Basic c2tfdGVzdF9NMU5EOUpzRWtCNVQyNUxCZ0hySEwyZmk6')
-            ->withData($data)
-            ->asJson()
-            ->withOption('SSL_VERIFYHOST', false)
-            ->withOption('SSL_VERIFYPEER', false)
-            ->post();
-
-
-        Session::put('session_id', $response->data->id);
-
-        return redirect()->to($response->data->attributes->checkout_url);
     }
 
     public function success(){
