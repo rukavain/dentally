3bf8c794c22e5572750e895c48e9a7755f31c02c
rukavain <magtoto599@gmail.com>
2024-11-20 22:48:27 +0800

    notification adjustment

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,6 +6,7 @@
     <title>{{ config('app.name') }} | Admin Dashboard </title>
     <link rel="icon" href="{{ asset('assets/images/logo.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{csrf_toke()}}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <script src="//unpkg.com/alpinejs" defer></script>
@@ -13,7 +14,8 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
-    @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
 <body class="">
